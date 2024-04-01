749b9e131f452fcd1f2acad454308aa18888162d
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-04-01 17:08:31 +0530

    connected to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const colors = require("colors")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log(`Connected to MongoDB...`.bgGreen);
+}).catch((err) =>{
+    console.log(err)
+});
 
 app.get("/health",(req,res) =>{
     res.status(200).json({message:"Everything is fine..."})
