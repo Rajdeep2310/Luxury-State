403b92222ef3d90a72b356257ac03fb5661bc363
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-04-06 19:24:36 +0530

    complete user listing delete function....

--- server/Controllers/listingController.js
+++ server/Controllers/listingController.js
@@ -10,6 +10,19 @@ const createListing = async (req,res,next) =>{
     }
 }
 
+const deleteListing = async(req,res,next) =>{
+    try{
+        const listing = await Listing.findById(req.params.id)
+        if(!listing) return next(errorHandler(404,"Listing not found.."))
+        if(req.user.id !== listing.userRef) return next(errorHandler(401,"You can delete your listings only..."))
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json({message:"Listing deleted succesfully...."})
+    }catch(error){
+        next(error)
+    }
+}
+
 module.exports = {
-    createListing
+    createListing,
+    deleteListing
 }
--- server/Routes/listingRoutes.js
+++ server/Routes/listingRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const listingRoutes = express.Router();
-const { createListing } = require("../Controllers/listingController");
+const { createListing , deleteListing } = require("../Controllers/listingController");
 const verifyToken = require("../utils/verifyToken");
 
 listingRoutes.post("/create",verifyToken,createListing)
+listingRoutes.delete("/delete/:id",verifyToken,deleteListing)
 
 module.exports = listingRoutes ;
